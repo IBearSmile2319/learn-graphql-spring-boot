c6b7ecd9045629961bb88686bf78f64acc15a0e5
Maicol Manuel Orosco Vasquez <78711486+IBearSmile2319@users.noreply.github.com>
2023-07-06 16:23:31 -0500

    init

---
+++ src/test/java/dev/maicol/learngraphqlspringboot/LearnGraphqlSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.maicol.learngraphqlspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGraphqlSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
